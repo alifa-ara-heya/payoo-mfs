0da70ed7fe32549e40f455bbd2d270e14e33115d
Alifa Ara Heya <alifaaraheya@gmail.com>
2024-09-19 00:37:03 -0400

    add money btn

--- js/login.js
+++ js/login.js
@@ -2,8 +2,21 @@
 document.getElementById("button-login").
     addEventListener('click', function(event){
         event.preventDefault(); //to stop reloading the page when submitting
-        console.log('testing');
-        // step-2: get the phone number
+
+        // step-2: get the phone and pin number
         const phoneNumber = document.getElementById("phone-number").value;
-        console.log(phoneNumber);
+        const pinNumber = document.getElementById("pin-number").value;
+        console.log(phoneNumber, pinNumber);
+
+        // validating phone and pin number
+        // following is temporary way
+        if (phoneNumber === '5' && pinNumber === "2"){
+            console.log("You are logged in");
+
+            // step-5: allow the user to use the website
+            window.location.href="./home.html";
+
+        } else{
+            alert('Wrong phone number or pin');
+        }
     })
